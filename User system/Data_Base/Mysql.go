40542670b8f20e901ff72fe3236bdf0b2330a383
Mreza <mreza_4040@outlook.com>
2025-09-26 23:09:01 +0330

    Smart Flight Management System v 0.2

--- User system/Data_Base/Mysql.go
+++ User system/Data_Base/Mysql.go
@@ -1,1 +1,24 @@
 package Data_Base
+
+import (
+	"User_system/Error_Handling"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"go.uber.org/zap"
+)
+
+func DBMysql() (*sql.DB, string) {
+	db, err := sql.Open("mysql", Error_Handling.LoadConfigFile().DBMysql.DB)
+	if err != nil {
+		zap.L().Error("not open mysql server", zap.String("err", err.Error()))
+		return nil, "err"
+	}
+	if err = db.Ping(); err != nil {
+		zap.L().Error("not ping", zap.String("err", err.Error()))
+		return nil, "err"
+
+	}
+	zap.L().Info("Server started successfully")
+	return db, ""
+}
--- User system/go.mod
+++ User system/go.mod
@@ -3,6 +3,8 @@ module User_system
 go 1.25.1
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
